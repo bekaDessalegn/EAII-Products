0cbde37308c2f5993d289c438c635407fe1ce5b4
Beka Dessalegn <39644079+bekaDessalegn@users.noreply.github.com>
2023-05-24 22:29:03 +0300

    made some changes

---
+++ hasura/seeds/default/1684938700449_adminsSeed.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.admin (id, username, email, password) VALUES ('fe65b836-b1e6-43ea-8b40-820813b5b223', 'Mg1', 'mg@gmail.com', '$2b$10$Tp9H9IFXLElb0tCddy279.bOL7U8AQr0qUHDEJuUSYrJl6H1lONrC');
+INSERT INTO public.admin (id, username, email, password) VALUES ('33f0930c-679d-4bce-9089-77fa1dffe3bf', 'Beka', 'beka@gmail.com', '$2b$10$SJ/t6lkOBwoXBo113P3rfOFDap8Nxw9Uca7.ZSjbY3kzCfIWRXmd.');
